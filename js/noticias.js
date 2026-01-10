9179da0591ce8238d3c774a04f05e8628eb53831
alvarofarrona <alvarofarronadelgado27@gmail.com>
2026-01-10 18:50:30 +0100

    giro noticias js

--- js/noticias.js
+++ js/noticias.js
@@ -5,45 +5,44 @@ $(".burger i").on("click", function () {
 });
 
 // NEWSLETTER VALIDACIÓN
-
 const form = document.getElementById("newsletter-form");
 const emailInput = document.getElementById("newsletter-email");
 
-const defaultPlaceholder = "Escribe tu email";
+if (form && emailInput) {
+  const defaultPlaceholder = "Escribe tu email";
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
 
-  const email = emailInput.value.trim();
+    const email = emailInput.value.trim();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  if (email === "") {
-    showError("Introduce tu email");
-    return;
+    if (email === "") {
+      showError("Introduce tu email");
+      return;
+    }
+
+    if (!emailRegex.test(email)) {
+      showError("Email no válido");
+      return;
+    }
+
+    emailInput.classList.remove("error");
+    emailInput.placeholder = defaultPlaceholder;
+    alert("¡Gracia's por suscribirte!");
+    form.reset();
+  });
+
+  emailInput.addEventListener("input", () => {
+    emailInput.classList.remove("error");
+    emailInput.placeholder = defaultPlaceholder;
+  });
+
+  function showError(message) {
+    emailInput.value = "";
+    emailInput.classList.add("error");
+    emailInput.placeholder = message;
   }
-
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  if (!emailRegex.test(email)) {
-    showError("Email no válido");
-    return;
-  }
-
-  // OK
-  emailInput.classList.remove("error");
-  emailInput.placeholder = defaultPlaceholder;
-  alert("¡Gracia's por suscribirte!");
-  form.reset();
-});
-
-emailInput.addEventListener("input", () => {
-  emailInput.classList.remove("error");
-  emailInput.placeholder = defaultPlaceholder;
-});
-
-function showError(message) {
-  emailInput.value = "";
-  emailInput.classList.add("error");
-  emailInput.placeholder = message;
 }
 
 // CURSOR
@@ -70,26 +69,8 @@ $("a").hover(
     $(".cursor").removeClass("cursor-grow");
   }
 );
-// Interacción opcional para "Leer más"
-$(".read-more").on("click", function (e) {
-  e.preventDefault();
-  alert(
-    "Esta funcionalidad puede abrir una ventana modal o redirigir a una noticia completa."
-  );
-});
-// MOSTRAR TEXTO EXTRA AL HACER CLICK
+
+// GIRO 3D POR CLICK
 $(".news-card").on("click", function () {
-  const extra = $(this).find(".news-extra");
-
-  // Si está cerrado → abrir
-  if (extra.css("max-height") === "0px") {
-    extra.css("max-height", "300px");
-  } else {
-    extra.css("max-height", "0px");
-  }
-});
-
-// OCULTAR TEXTO EXTRA AL SALIR DEL CUADRADO
-$(".news-card").on("mouseleave", function () {
-  $(this).find(".news-extra").css("max-height", "0px");
+  $(this).toggleClass("flip");
 });
